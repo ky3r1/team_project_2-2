ab991b3d3ecd50411e1199d797831c976e26bace
隼人 津守 <2230121@ecc.ac.jp>
2024-05-24 12:23:11 +0900

    a

--- Source/CameraController.cpp
+++ Source/CameraController.cpp
@@ -37,22 +37,22 @@ void CameraController::Update(float elapsedTime)
     //    angle.y -= DirectX::XM_2PI;
     //}
 
-    ////カメラ回転値を回転行列に変換
-    //DirectX::XMMATRIX Transform = DirectX::XMMatrixRotationRollPitchYaw(angle.x, angle.y, angle.z);
+    //カメラ回転値を回転行列に変換
+    DirectX::XMMATRIX Transform = DirectX::XMMatrixRotationRollPitchYaw(angle.x, angle.y, angle.z);
 
-    ////回転行列から前方向ベクトルを取り出す
-    //DirectX::XMVECTOR Front = Transform.r[2];
-    //DirectX::XMFLOAT3 front;
-    //DirectX::XMStoreFloat3(&front, Front);
+    //回転行列から前方向ベクトルを取り出す
+    DirectX::XMVECTOR Front = Transform.r[2];
+    DirectX::XMFLOAT3 front;
+    DirectX::XMStoreFloat3(&front, Front);
 
-    ////注視点から後ろベクトル方向に一定距離離れたカメラ視点を求める
-    //DirectX::XMFLOAT3 eye;
-    //eye.x = target.x - front.x * range / 1.0f;
-    //eye.y = target.y - front.y * range / 1.0f;
-    //eye.z = target.z - front.z * range / 1.0f;
+    //注視点から後ろベクトル方向に一定距離離れたカメラ視点を求める
+    DirectX::XMFLOAT3 eye;
+    eye.x = target.x - front.x * range / 1.0f;
+    eye.y = target.y - front.y * range / 1.0f;
+    eye.z = target.z - front.z * range / 1.0f;
 
-    ////カメラの視点と注視点を設定
-    //Camera::Instance().SetLookAt(eye, target, DirectX::XMFLOAT3(0, 1, 0));
+    //カメラの視点と注視点を設定
+    Camera::Instance().SetLookAt(eye, target, DirectX::XMFLOAT3(0, 1, 0));
 }
 
 
