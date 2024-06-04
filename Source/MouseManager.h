fc1a43c32ecc3c4cd0475fc05c464498cff1e140
颯磨 田中 <2230151@ecc.ac.jp>
2024-06-04 11:12:51 +0900

    mouse

--- Source/MouseManager.h
+++ Source/MouseManager.h
@@ -7,7 +7,10 @@
 class MouseManager
 {
     void MouseTransform(ID3D11DeviceContext* dc, const DirectX::XMFLOAT4X4& view, const DirectX::XMFLOAT4X4& projection);
-
+    MouseManager* GetInstance() {
+        static MouseManager instance;
+        return &instance;
+    }
 
 private:
     GamePad& gamePad = Input::Instance().GetGamePad();
