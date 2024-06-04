a4f070a91abedec24f9307174254c66d1cc671d6
隼人 津守 <2230121@ecc.ac.jp>
2024-06-04 10:21:15 +0900

    a

--- Source/MouseManager.cpp
+++ Source/MouseManager.cpp
@@ -3,7 +3,8 @@
 void MouseManager::MouseTransform(ID3D11DeviceContext* dc, const DirectX::XMFLOAT4X4& view, const DirectX::XMFLOAT4X4& projection)
 {
     //ビューポート
-    numViewports = 1;
+    D3D11_VIEWPORT viewport;
+    UINT numViewports = 1;
     dc->RSGetViewports(&numViewports, &viewport);
 
     //変換行列
@@ -41,33 +42,32 @@ void MouseManager::MouseTransform(ID3D11DeviceContext* dc, const DirectX::XMFLOA
     DirectX::XMFLOAT3 world_position_start;
     DirectX::XMStoreFloat3(&world_position_start, WorldPosition);
 
+    screenPosition.z = 1;
+    ScreenCursor = DirectX::XMLoadFloat3(&screenPosition);
+    WorldPosition = DirectX::XMVector3Unproject
+    (
+    	ScreenCursor,
+    	viewport.TopLeftX,
+    	viewport.TopLeftY,
+    	viewport.Width,
+    	viewport.Height,
+    	viewport.MinDepth,
+    	viewport.MaxDepth,
+    	Projection,
+    	View,
+    	World
+    );
+    DirectX::XMFLOAT3 world_position_end;
+    DirectX::XMStoreFloat3(&world_position_end, WorldPosition);
 
-    //screenPosition.z = 1;
-    //ScreenCursor = DirectX::XMLoadFloat3(&screenPosition);
-    //WorldPosition = DirectX::XMVector3Unproject
-    //(
-    //    ScreenCursor,
-    //    viewport.TopLeftX,
-    //    viewport.TopLeftY,
-    //    viewport.Width,
-    //    viewport.Height,
-    //    viewport.MinDepth,
-    //    viewport.MaxDepth,
-    //    Projection,
-    //    View,
-    //    World
-    //);
-    //DirectX::XMFLOAT3 world_position_end;
-    //DirectX::XMStoreFloat3(&world_position_end, WorldPosition);
-
-    //HitResult hit;
-    //StageMain stage_main;
-    //if (stage_main.RayCast(world_position_start, world_position_end, hit))
-    //{
-    //    /*EnemyManager& enemyManager = EnemyManager::Instance();
-    //    EnemySlime* slime = new EnemySlime(GREEN, 0);
-    //    slime->SetPosition(DirectX::XMFLOAT3(world_position_start.x, world_position_start.y, world_position_start.z));
-    //    enemyManager.Register(slime);*/
-    //}
+    HitResult hit;
+    StageMain stage_main;
+    if (stage_main.RayCast(world_position_start, world_position_end, hit))
+    {
+    /*EnemyManager& enemyManager = EnemyManager::Instance();
+    EnemySlime* slime = new EnemySlime(GREEN, 0);
+    slime->SetPosition(DirectX::XMFLOAT3(world_position_start.x, world_position_start.y, world_position_start.z));
+    enemyManager.Register(slime);*/
+    }
     //}
 }
